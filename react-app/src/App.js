c199ae2e7a2653ad44db418271cf35f76c0f8f12
Sh0ri <quent.dasilva@gmail.com>
2018-04-23 14:11:18 +0200

    Revert "call parent function"
    This reverts commit c16a5a1dabd979ac58bfffea441d6b90a8d7f083.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import ScrollUpButton from "react-scroll-up-button";
 
+<<<<<<< HEAD
 class NavBar extends Component {
   constructor(props) {
     super(props);
@@ -131,8 +132,21 @@ class App extends Component {
       {this.renderElem1()}
       {this.renderElem2()}
       <ScrollUpButton />
+=======
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+>>>>>>> parent of c16a5a1... call parent function
       </div>
-      );
+    );
   }
 }
 
