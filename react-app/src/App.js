c16a5a1dabd979ac58bfffea441d6b90a8d7f083
Sh0ri <quent.dasilva@gmail.com>
2018-04-23 12:01:59 +0200

    call parent function

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,19 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value:"null",
+    };
+  }
+
+  Update(){
+    this.props.parentMethod(this.props.value);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Test">
+      <button onClick={() => this.Update()} >TEST {this.props.value}</button>
       </div>
-    );
+      );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number:[0,1],
+    };
+
+    this.AddOne = this.AddOne.bind(this);
+  }
+  renderTest(i) {
+    return <Test value={i} parentMethod={this.AddOne}/>;
+  }
+  AddOne(i) {
+    console.log("Add one called");
+    var tempState = this.state;
+    console.log(tempState);
+    tempState.number.push(i);
+    this.setState(tempState);
+    //number.map(temp=> console.log(temp));
+    //number.push(i);
+    //console.log(number);
+    //this.setState({ number: number })
+  }
+
+  render() {
+    const { number } = this.state;
+    return (
+      <div className="App">
+      {number.map(temp => this.renderTest(temp))}
+      </div>
+      );
   }
 }
 
